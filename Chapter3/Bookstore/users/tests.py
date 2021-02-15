ea642092487f0f33fe0f73d66c07c38ff2a40f47
vr3n <vickyspatel@gmail.com>
2021-02-15 17:28:22 +0530

    Created custom user model, form and integrated it with admin

--- Chapter3/Bookstore/users/tests.py
+++ Chapter3/Bookstore/users/tests.py
@@ -1,3 +1,33 @@
+from django.contrib.auth import get_user, get_user_model
 from django.test import TestCase
 
 # Create your tests here.
+
+class CustomUserTests(TestCase):
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username="Viren",
+            email="virenpatel@gmail.com",
+            password="Test@1234"
+        )
+
+        self.assertEqual(user.username, 'Viren')
+        self.assertEqual(user.email, 'virenpatel@gmail.com')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(
+            username="superuser",
+            email="superuser@gmail.com",
+            password="test@1234"
+        )
+
+        self.assertEqual(admin_user.username, 'superuser')
+        self.assertEqual(admin_user.email, 'superuser@gmail.com')
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
