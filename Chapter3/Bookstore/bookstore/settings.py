a9135946417aada124ee1215377cb0042f951509
vr3n <vickyspatel@gmail.com>
2021-02-18 18:59:41 +0530

    Created Environment Variables

--- Chapter3/Bookstore/bookstore/settings.py
+++ Chapter3/Bookstore/bookstore/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'th&*29iw9rtvg%ex4b1sr&eq_#7-c(@eeikzfgzv=dy830jr8r'
+SECRET_KEY = os.environ.get('NEW_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', default=0))
 
 ALLOWED_HOSTS = []
 
