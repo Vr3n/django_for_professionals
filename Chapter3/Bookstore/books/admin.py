42f4256fc5e7b00c0f43e69f1a1a0935ec6f2b2d
vr3n <vickyspatel@gmail.com>
2021-02-18 20:36:35 +0530

    Created Reviews app, now users can review the books

--- Chapter3/Bookstore/books/admin.py
+++ Chapter3/Bookstore/books/admin.py
@@ -1,7 +1,11 @@
-from books.models import Book
+from books.models import Book, Review
 from django.contrib import admin
 
 # Register your models here.
+
+class ReviewInline(admin.TabularInline):
+    model = Review
+
 
 class BookAdmin(admin.ModelAdmin):
     list_display = (
@@ -9,5 +13,7 @@ class BookAdmin(admin.ModelAdmin):
         'author',
         'price',
     )
+
+    inlines = [ReviewInline, ]
 
 admin.site.register(Book, BookAdmin)
